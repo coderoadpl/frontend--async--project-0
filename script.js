223574b1467e0cbce8d6ac332988de6c131b2424
Mateusz Choma <kontakt@coderoad.pl>
2020-11-27 17:04:11 +0100

    Custom fetch callbacks

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-const fetchData = (url) => {
-    console.log('Start fetching')
+const fetchData = (url, options = {}) => {
+    const {
+        startCallback = () => console.log(`[${url}] Start fetching`),
+        catchCallback = (error) => console.error('Failed to fetch data', error),
+        endCallback = () => console.log(`[${url}] Stop fetching`)
+    } = options
+
+    startCallback()
     return fetch(url)
         .then((response) => response.json())
-        .catch(() => console.error('Failed to fetch data'))
-        .finally(() => console.log('Stop fetching'))
+        .catch(catchCallback)
+        .finally(endCallback)
 }
 
 fetchData('/data.json')
     .then((data) => console.log(data))
 
-fetchData('/data2.json')
+fetchData('/data2.json', { startCallback: () => console.log('Start fetching data2.json') })
     .then((data) => console.log(data))

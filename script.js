11628ab92df7d0420570e76e3f3848add21d3816
Mateusz Choma <kontakt@coderoad.pl>
2020-11-27 17:29:56 +0100

    Loading state added

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ class ToDo {
     constructor() {
         this.container = document.body
         this.tasks = []
+        this.isLoading = true
+        this.hasError = null
     }
 
     init() {
-        fetchData('/data.json')
+        fetchData('/data.json', {
+            endCallback: this.endLoadingTasks.bind(this)
+        })
             .then(this.loadTasks.bind(this))
 
+        this.render()
+    }
+
+    endLoadingTasks() {
+        this.isLoading = false
         this.render()
     }
 
@@ -18,6 +27,14 @@ class ToDo {
 
     render() {
         this.container.innerHTML = ''
+
+        if (this.isLoading) {
+            const text = document.createElement('p')
+            text.innerText = 'Loading...'
+            this.container.appendChild(text)
+
+            return
+        }
 
         const ul = document.createElement('ul')
 
